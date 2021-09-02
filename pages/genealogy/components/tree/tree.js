15ef320d4e8815562291dff6d659baadfe8aac32
shenxh <xianhui.shen@eslink.com>
2021-09-02 08:43:13 +0800

    feat: 修改树状图

--- pages/genealogy/components/tree/tree.js
+++ pages/genealogy/components/tree/tree.js
@@ -2,7 +2,7 @@
  * @Author: shenxh
  * @Date: 2021-08-31 09:46:10
  * @LastEditors: shenxh
- * @LastEditTime: 2021-09-01 14:59:33
+ * @LastEditTime: 2021-09-02 08:42:01
  * @Description: 树形图
  */
 import * as echarts from '../../../../ec-canvas/echarts';
@@ -37,17 +37,23 @@ function initChart(canvas, width, height, dpr) {
       {
         name: '族谱图',
         type: 'tree',
-        orient: 'horizontal', // vertical horizontal
-        left: '20%',
-        right: '-200%',
-        top: '-500%',
-        bottom: '-700%',
+        // orient: 'horizontal', // vertical horizontal
+        // left: '20%',
+        // right: '-200%',
+        // top: '-500%',
+        // bottom: '-700%',
+        orient: 'vertical', // vertical horizontal
+        left: '-1000%',
+        right: '-1400%',
+        top: '10%',
+        bottom: '-200%',
         rootLocation: {
           x: 'center',
-          y: '15%'
+          y: 'center'
         }, // 根节点位置  {x: 'center',y: 10}
         symbol: 'rect',
-        symbolSize: [60, 25],
+        // symbolSize: [60, 25],
+        symbolSize: [25, 60],
         roam: true,
         expandAndCollapse: false,
         initialTreeDepth: -1, // 展开层级
@@ -84,6 +90,7 @@ function setAncestors(list) {
   if (list && list.length) {
     tmp = list.map(item => {
       return Object.assign({}, item, {
+        name: item.name.split('').join('\n'),
         itemStyle: {
           color: item.star ? '#f56949' : '#3496eb'
         },
