8b01490c709991cb25d23bb34607723652d8c838
shenxh <xianhui.shen@eslink.com>
2022-04-22 09:59:12 +0800

    feat: 上传微信小程序模板

--- .prettierrc.js
+++ .prettierrc.js
@@ -4,6 +4,6 @@ module.exports = {
   semi: true, // 使用分号
   singleQuote: true, // 使用单引号
   arrowParens: 'avoid', // 箭头函数参数括号 avoid|always
-  trailingComma: 'none', // 行尾逗号 none|es5|all
+  trailingComma: 'all', // 行尾逗号 none|es5|all
   htmlWhitespaceSensitivity: 'css'
 };
