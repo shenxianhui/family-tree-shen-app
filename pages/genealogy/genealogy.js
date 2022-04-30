12346f39c7d3a746c2bde3476326d083887c0e59
shenxh <shenxh0928@qq.com>
2022-04-30 15:47:37 +0800

    feat: 图表修改为手动加载

--- pages/genealogy/genealogy.js
+++ pages/genealogy/genealogy.js
@@ -2,95 +2,74 @@
  * @Author: shenxh
  * @Date: 2021-08-31 09:46:10
  * @LastEditors: shenxh
- * @LastEditTime: 2022-04-30 14:13:58
+ * @LastEditTime: 2022-04-30 15:46:31
  * @Description: 树形图
  */
 import * as echarts from '../../ec-canvas/echarts';
 import ancestors from '../../assets/data/ancestors-tree';
 
-function initChart(canvas, width, height, dpr) {
-  const chart = echarts.init(canvas, null, {
-    width: width,
-    height: height,
-    devicePixelRatio: dpr, // new
-  });
-  canvas.setChart(chart);
-
-  chart.on('click', evt => {
-    wx.navigateTo({
-      url: '/pages/user-details/user-details',
-      success: res => {
-        res.eventChannel.emit('data', evt.data);
+let myChart = null;
+let option = {
+  // backgroundColor: new echarts.graphic.RadialGradient(0.4, 0.4, 0.8, [
+  //   {
+  //     offset: 0,
+  //     color: '#f7f8fa',
+  //   },
+  //   {
+  //     offset: 1,
+  //     color: '#cdd0d5',
+  //   },
+  // ]),
+  series: [
+    {
+      // zoom: 0.5,
+      name: '族谱图',
+      type: 'tree',
+      left: '20%',
+      right: '-300%',
+      top: '-870%',
+      bottom: '-1050%',
+      orient: 'horizontal', // vertical horizontal
+      rootLocation: {
+        x: 'center',
+        y: 'center',
+      }, // 根节点位置  {x: 'center',y: 10}
+      symbol: 'none',
+      symbolSize: false,
+      roam: 'move', // 是否开启鼠标缩放和平移漫游 可以设置成 'scale' 或者 'move'。设置成 true 为都开启
+      expandAndCollapse: false,
+      initialTreeDepth: -1, // 展开层级
+      edgeShape: 'polyline', // curve polyline
+      label: {
+        show: true,
+        position: 'inside',
+        // fontWeight: 'bold',
+        // fontFamily: 'STLiti', // 华文隶书: STLiti; 楷体: KaiTi
+        textStyle: {
+          color: '#fff',
+          fontSize: 18,
+        },
+        width: 70,
+        height: 30,
+        backgroundColor: '#3496eb',
       },
-    });
-  });
-
-  let ancestorsData = setAncestors([ancestors]);
-
-  var option = {
-    // backgroundColor: new echarts.graphic.RadialGradient(0.4, 0.4, 0.8, [
-    //   {
-    //     offset: 0,
-    //     color: '#f7f8fa',
-    //   },
-    //   {
-    //     offset: 1,
-    //     color: '#cdd0d5',
-    //   },
-    // ]),
-    series: [
-      {
-        // zoom: 0.5,
-        name: '族谱图',
-        type: 'tree',
-				left: '20%',
-				right: '-300%',
-				top: '-870%',
-				bottom: '-1050%',
-        orient: 'horizontal', // vertical horizontal
-        rootLocation: {
-          x: 'center',
-          y: 'center',
-        }, // 根节点位置  {x: 'center',y: 10}
-        symbol: 'none',
-        symbolSize: false,
-        roam: 'move', // 是否开启鼠标缩放和平移漫游 可以设置成 'scale' 或者 'move'。设置成 true 为都开启
-        expandAndCollapse: false,
-        initialTreeDepth: -1, // 展开层级
-        edgeShape: 'polyline', // curve polyline
-        label: {
-          show: true,
-          position: 'inside',
-          // fontWeight: 'bold',
-          // fontFamily: 'STLiti', // 华文隶书: STLiti; 楷体: KaiTi
-          textStyle: {
-            color: '#fff',
-            fontSize: 18,
-          },
-          width: 70,
-          height: 30,
-          backgroundColor: '#3496eb',
-        },
-        labelLayout: {
-          hideOverlap: true, // 隐藏重叠的标签
-        },
-        lineStyle: {
-          color: '#3496eb',
-          curveness: 0.5,
-          width: 1,
-          type: 'solid',
-        },
-        data: ancestorsData,
+      labelLayout: {
+        hideOverlap: true, // 隐藏重叠的标签
       },
-    ],
-    animation: false,
-  };
+      lineStyle: {
+        color: '#3496eb',
+        curveness: 0.5,
+        width: 1,
+        type: 'solid',
+      },
+      data: initTreeData([ancestors]),
+    },
+  ],
+  animation: false,
+};
 
-  chart.setOption(option);
-  return chart;
-}
-
-function setAncestors(list, parent = {}) {
+// 数据格式化
+function initTreeData(list, parent = {}) {
   let tmp = [];
 
   if (list && list.length) {
@@ -116,7 +95,7 @@ function setAncestors(list, parent = {}) {
         label: {
           backgroundColor: color,
         },
-        children: setAncestors(item.children, item),
+        children: initTreeData(item.children, item),
       });
     });
   }
@@ -127,11 +106,20 @@ function setAncestors(list, parent = {}) {
 Page({
   data: {
     ec: {
-      onInit: initChart,
+      // 将 lazyLoad 设为 true 后，需要手动初始化图表
+      lazyLoad: true,
     },
   },
 
-  onReady() {},
+  // 页面首次渲染完毕时执行
+  onReady() {
+    this.initChart();
+  },
+
+  // 页面销毁时执行
+  onUnload() {
+    this.dispose();
+  },
 
   // 页面被用户分享时执行
   onShareAppMessage() {
@@ -149,9 +137,57 @@ Page({
     };
   },
 
+  // 返回按钮
   handleBack() {
     wx.switchTab({
       url: '/pages/home/home',
     });
+    // Object.assign(option.series[0], {
+    //   left: '20%',
+    //   right: '-300%',
+    //   top: '-870%',
+    //   bottom: '-1050%',
+    // });
+
+    // this.initChart();
+  },
+
+  // 图表初始化
+  initChart() {
+    this.dispose();
+    // 获取组件
+    const ecComponent = this.selectComponent('#chart-tree');
+
+    ecComponent.init((canvas, width, height, devicePixelRatio) => {
+      // 获取组件的 canvas、width、height 后的回调函数
+      // 在这里初始化图表
+      myChart = echarts.init(canvas, null, {
+        width,
+        height,
+        devicePixelRatio, // new
+      });
+
+      myChart.on('click', evt => {
+        wx.navigateTo({
+          url: '/pages/user-details/user-details',
+          success: res => {
+            res.eventChannel.emit('data', evt.data);
+          },
+        });
+      });
+
+      myChart.setOption(option);
+
+      // 注意这里一定要返回 myChart 实例，否则会影响事件处理等
+      return myChart;
+    });
+  },
+
+  // 销毁实例
+  dispose() {
+    if (myChart) {
+      myChart.dispose();
+      myChart = null;
+    }
   },
 });
